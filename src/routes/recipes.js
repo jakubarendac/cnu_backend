9a94047a68a0d31570726bc8cc0113ff2be94f95
Jiri Rejman <rejman@cngroup.dk>
2021-10-26 13:04:52 +0200

    - added route with parameter and validation

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,10 +1,28 @@
 import express from 'express'
+const { param, validationResult } = require('express-validator');
 
 const router = express.Router()
 
 router.get('/', async (req, res) => {
     try {
         const allRecipes = await req.context.models.recipe.findAll({
+            include: [{ model: req.context.models.ingredient }]
+        })
+        return res.send(allRecipes)
+    } catch (err) {
+        res.status(400).send('something failed... ')
+    }
+})
+
+router.get('/:recipeTitle', param('recipeTitle').isLength({ max: 25 }).isLength({ min: 3 }), async (req, res) => {
+    try {
+        const result = validationResult(req)
+        if (!result.isEmpty()) {
+            res.status(400).send('The request must be between 3 and 25 characters long')
+            return
+        }
+        const allRecipes = await req.context.models.recipe.findAll({
+            where: { title: req.params.recipeTitle },
             include: [{ model: req.context.models.ingredient }]
         })
         return res.send(allRecipes)
